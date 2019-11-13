bb10580f9786910ee65903959e2c09e81d5fb42e
Roux JP <jproux343@gmail.com>
2019-11-13 02:05:53 +0100

    TP03 : Fichier conf, filtering, profils

--- premier-projet-maven/src/main/java/dev/App.java
+++ premier-projet-maven/src/main/java/dev/App.java
@@ -1,6 +1,6 @@
 package dev;
 import com.github.lalyos.jfiglet.FigletFont;
-
+import java.util.ResourceBundle;
 /**
  * Hello world!
  *
@@ -11,9 +11,19 @@ public class App
     {
         /** Avant
     	System.out.println( "Hello World!" );
-    	**/
+    	
  
     	String asciiArt = FigletFont.convertOneLine("hello");
     	System.out.println(asciiArt);
+    	**/
+    	
+    	// récupération du titre depuis le fichier de propriété.
+        String titre = ResourceBundle.getBundle("application").getString("titre"); 
+        String asciiArt = FigletFont.convertOneLine(titre);
+        System.out.println(asciiArt);
+        
+        // affichage de l'environnement
+        String environnement = ResourceBundle.getBundle("application").getString("environnement");
+        System.out.println("Environnement : " + environnement);
     }
 }
